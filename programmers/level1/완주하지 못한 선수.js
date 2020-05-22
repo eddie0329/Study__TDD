b0540b25aa933b63823f84839c7038e1647c8487
eddie0329 <fdsa0329@naver.com>
2020-05-22 22:54:14 +0900

    :recycle: Refactor codes

--- programmers/level1/완주하지 못한 선수.js
+++ programmers/level1/완주하지 못한 선수.js
@@ -1,1 +1,19 @@
-export function solution(participants, completions) {}
+const isNotParticipant = (participant, completion) => {
+  return participant !== completion;
+};
+
+const searchUnparticipant = (participants, completions) => {
+  for (let i = 0; i < participants.length; i++) {
+    if (isNotParticipant(participants[i], completions[i])) {
+      return participants[i];
+    }
+  }
+};
+
+const sortArray = (array) => {
+  return array.sort();
+};
+
+export function solution(participants, completions) {
+  return searchUnparticipant(sortArray(participants), sortArray(completions));
+}
--- programmers/level1/완주하지 못한 선수.test.js
+++ programmers/level1/완주하지 못한 선수.test.js
@@ -1,16 +1,22 @@
 import { solution } from "./완주하지 못한 선수";
 
-test("완주하지 못한 선수 test", () => {
-  const test1 = solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
-  expect(test1).toEqual("leo");
-  const test2 = solution(
-    ["marina", "josipa", "nikola", "vinko", "filipa"],
-    ["josipa", "filipa", "marina", "nikola"]
-  );
-  expect(test2).toEqual("vinko");
-  const test3 = solution(
-    ["mislav", "stanko", "mislav", "ana"],
-    ["stanko", "ana", "mislav"]
-  );
-  expect(test3).toEqual("mislav");
+describe("완주하지 못한 선수 test", () => {
+  it("테스트1", () => {
+    const test1 = solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
+    expect(test1).toEqual("leo");
+  });
+  it("테스트2", () => {
+    const test2 = solution(
+      ["marina", "josipa", "nikola", "vinko", "filipa"],
+      ["josipa", "filipa", "marina", "nikola"]
+    );
+    expect(test2).toEqual("vinko");
+  });
+  it("테스트3", () => {
+    const test3 = solution(
+      ["mislav", "stanko", "mislav", "ana"],
+      ["stanko", "ana", "mislav"]
+    );
+    expect(test3).toEqual("mislav");
+  });
 });
