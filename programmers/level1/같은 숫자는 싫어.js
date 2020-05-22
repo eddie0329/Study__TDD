a0b2fd0dd820559e00f9a4a5f67e596e7afb663f
eddie0329 <fdsa0329@naver.com>
2020-05-22 23:02:34 +0900

    :sparkles: Solve Problems

--- programmers/level1/같은 숫자는 싫어.js
+++ programmers/level1/같은 숫자는 싫어.js
@@ -1,1 +1,11 @@
-export function solution(arr) {}
+export function solution(arr) {
+  let prev = null;
+  const answer = [];
+  arr.forEach((num) => {
+    if (num !== prev) {
+      answer.push(num);
+      prev = num;
+    }
+  });
+  return answer;
+}
--- programmers/level1/같은 숫자는 싫어.test.js
+++ programmers/level1/같은 숫자는 싫어.test.js
@@ -1,6 +1,6 @@
 import { solution } from "./같은 숫자는 싫어";
 
-decribe("같은 숫자는 싫어 테스트", () => {
+describe("같은 숫자는 싫어 테스트", () => {
   it("테스트1", () => {
     const test1 = solution([1, 1, 3, 3, 0, 1, 1]);
     expect(test1).toEqual([1, 3, 0, 1]);
