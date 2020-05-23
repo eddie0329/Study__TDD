7ce206b4db7379989dc159cec24c0a2d2b4d070a
eddie <fdsa0329@naver.com>
2020-05-23 21:05:33 +0900

    :recycle: Refactor solution

--- programmers/level1/두 정수 사이의 합.js
+++ programmers/level1/두 정수 사이의 합.js
@@ -8,9 +8,6 @@ export const makeNewArray = (start, end) => {
 
 export function solution(a, b) {
   const array = Array.of(a, b).sort((a, b) => a - b);
-  const newArray = [];
-  for (let i = array[0]; i <= array[1]; i++) {
-    newArray.push(i);
-  }
+  const newArray = makeNewArray(array[0], array[1]);
   return newArray.reduce((a, c) => a + c);
 }
