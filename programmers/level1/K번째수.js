16c8d07091fa0f1f6732afae67e01736a1292cbf
eddie0329 <fdsa0329@naver.com>
2020-05-28 22:02:34 +0900

    :beers: Solve solution

--- programmers/level1/K번째수.js
+++ programmers/level1/K번째수.js
@@ -1,1 +1,19 @@
-export function solution(array, commands) {}
+export function solution(array, commands) {
+  const answer = [];
+  let start, end, find, temp;
+  commands.forEach((command) => {
+    command.forEach((direction, index) => {
+      if (index === 0) {
+        start = direction - 1;
+      } else if (index === 1) {
+        end = direction;
+      } else {
+        find = direction - 1;
+      }
+    });
+    temp = array.slice(start, end);
+    temp.sort((a, b) => a - b);
+    answer.push(temp[find]);
+  });
+  return answer;
+}
