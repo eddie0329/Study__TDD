2dc2a852c6708836dd6eaf1e3a0362e102d488c6
eddie0329 <fdsa0329@naver.com>
2020-05-28 21:56:56 +0900

    :white_check_mark: Add test

---
+++ programmers/level1/K번째수.test.js
@@ -0,0 +1,15 @@
+import { solution } from "./K번째수";
+
+describe("K번째수 test", () => {
+  it("solution test", () => {
+    const array = [1, 5, 2, 6, 3, 7, 4];
+    const commands = [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3],
+    ];
+    const answer = [5, 6, 3];
+
+    expect(solution(array, commands)).toEqual(answer);
+  });
+});
