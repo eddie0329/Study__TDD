b3d00822e8080409ae4733e63ca18517e476a686
eddie0329 <fdsa0329@naver.com>
2020-05-29 21:04:44 +0900

    :recycle: Refactor solution

--- programmers/level1/핸드폰 번호 가리기.js
+++ programmers/level1/핸드폰 번호 가리기.js
@@ -1,10 +1,16 @@
+export const makeArray = (length) => {
+  return new Array(length);
+};
+
 export function solution(phone_number) {
-  const array = new Array(phone_number.length).fill("*").map((char, index) => {
-    if (phone_number.length - 4 <= index && index < phone_number.length) {
-      return phone_number[index];
-    } else {
-      return char;
-    }
-  });
+  const array = makeArray(phone_number.length)
+    .fill("*")
+    .map((char, index) => {
+      if (phone_number.length - 4 <= index && index < phone_number.length) {
+        return phone_number[index];
+      } else {
+        return char;
+      }
+    });
   return array.join("");
 }
--- programmers/level1/핸드폰 번호 가리기.test.js
+++ programmers/level1/핸드폰 번호 가리기.test.js
@@ -1,8 +1,13 @@
-import { solution } from "./핸드폰 번호 가리기";
+import { solution, makeArray } from "./핸드폰 번호 가리기";
 
 describe("핸드폰 번호 가리기", () => {
   it("solution test", () => {
     expect(solution("01033334444")).toBe("*******4444");
     expect(solution("027778888")).toBe("*****8888");
+  });
+  it("makeArray test", () => {
+    expect(makeArray(3).length).toBe(3);
+    expect(makeArray(4).length).toBe(4);
+    expect(makeArray(5).length).toBe(5);
   });
 });
