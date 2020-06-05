29195f7deaed2ca4cc83db0e4a2b79e3064bbc86
eddie <fdsa0329@naver.com>
2020-06-06 00:24:45 +0900

    :sparkles: Add fib problem

--- programmers/level2/피보나치 수.js
+++ programmers/level2/피보나치 수.js
@@ -1,4 +1,15 @@
 export function solution(n) {
-  const answer = 0;
-  return answer;
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  const arr = new Array(n + 1);
+  arr.splice(0, 1, 0);
+  arr.splice(1, 1, 1);
+
+  for (let i = 2; i < arr.length; i++) {
+    const result = arr[i - 1] + arr[i - 2];
+    arr.splice(i, 1, result);
+  }
+  return arr[n];
 }
