4cf1512e84432aaf8653474bf1dfde273910af86
soyoung <soyoungp02@gmail.com>
2020-06-06 01:56:29 +0900

    ✨ Solve Problems

--- programmers_소영/level1/K번째수.js
+++ programmers_소영/level1/K번째수.js
@@ -1,1 +1,3 @@
-export function solution(array, commands) {}
+export function solution(array, commands) {
+  return commands.map(([from, to, k]) => array.slice(from - 1, to).sort((a, b) => a - b)[k - 1]);
+}
