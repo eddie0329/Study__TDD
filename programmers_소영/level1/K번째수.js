5cf5843d04c28a64d8a6f1fcfdae6e4cf30ab048
soyoung <soyoungp02@gmail.com>
2020-06-06 01:59:29 +0900

    ♻️ Refactor solution

--- programmers_소영/level1/K번째수.js
+++ programmers_소영/level1/K번째수.js
@@ -1,3 +1,15 @@
 export function solution(array, commands) {
-  return commands.map(([from, to, k]) => array.slice(from - 1, to).sort((a, b) => a - b)[k - 1]);
+  return commands.map(([from, to, k]) => pickKth(sortAsc(sliceArray(array, from, to)), k));
+}
+
+export function sliceArray(array, from, to) {
+  return array.slice(from - 1, to);
+}
+
+export function sortAsc(array) {
+  return array.sort((a, b) => a - b);
+}
+
+export function pickKth(array, k) {
+  return array[k - 1];
 }
--- programmers_소영/level1/K번째수.test.js
+++ programmers_소영/level1/K번째수.test.js
@@ -1,4 +1,4 @@
-import { solution } from "./K번째수"
+import { solution, sliceArray, sortAsc, pickKth } from "./K번째수"
 
 describe('K번째수 test', () => {
   test('solution test', () => {
@@ -7,5 +7,30 @@ describe('K번째수 test', () => {
     const answer = [5, 6, 3];
 
     expect(solution(array, commands)).toEqual(answer);
+  });
+
+  test('sliceArray test', () => {
+    const array = [1, 5, 2, 6, 3, 7, 4];
+    expect(sliceArray(array, 2, 5)).toEqual([5, 2, 6, 3]);
+    expect(sliceArray(array, 4, 4)).toEqual([6]);
+    expect(sliceArray(array, 1, 7)).toEqual([1, 5, 2, 6, 3, 7, 4]);
+  });
+
+  test('sortAsc test', () => {
+    const array1 = [5, 2, 6, 3];
+    const array2 = [6];
+    const array3 = [1, 5, 2, 6, 3, 7, 4];
+    expect(sortAsc(array1)).toEqual([2, 3, 5, 6]);
+    expect(sortAsc(array2)).toEqual([6]);
+    expect(sortAsc(array3)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+
+  test('pickKth test', () => {
+    const array1 = [2, 3, 5, 6];
+    const array2 = [6];
+    const array3 = [1, 2, 3, 4, 5, 6, 7];
+    expect(pickKth(array1, 3)).toEqual(5);
+    expect(pickKth(array2, 1)).toEqual(6);
+    expect(pickKth(array3, 3)).toEqual(3);
   });
 })
