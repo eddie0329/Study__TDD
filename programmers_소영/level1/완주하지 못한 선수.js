5e6264ccd58f3a94e669ec9c57b1787b0485f2de
soyoung <soyoungp02@gmail.com>
2020-06-08 01:45:03 +0900

    ♻️ Refactor solution

--- programmers_소영/level1/완주하지 못한 선수.js
+++ programmers_소영/level1/완주하지 못한 선수.js
@@ -1,12 +1,17 @@
 export function solution(participant, completion) {
-    const completionObj = completion.reduce((obj, player) => {
-        obj[player] = obj[player] ? obj[player] + 1 : 1;
-        return obj;
-    }, {});
+    const completionObj = completion.reduce((obj, player) => ({ ...obj, [player]: getPlayerCount(obj, player) }), {});
+    return findUncompletePlayer(completionObj, participant);
+}
+
+export function getPlayerCount(obj, player) {
+    if (!obj[player])
+        return 1;
+    return obj[player] + 1;
+}
+
+export function findUncompletePlayer(completionObj, participant) {
     return participant.find(player => {
-        if(completionObj[player])
-            completionObj[player] = completionObj[player]-1;
-        else 
-            return true;
+        if (!completionObj[player]) return true;
+        completionObj[player] = completionObj[player] - 1;
     });
 }
