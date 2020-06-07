348e066a172b55ae579da6c226f81649610d500b
soyoung <soyoungp02@gmail.com>
2020-06-07 23:54:32 +0900

    ✅ Add solution test

---
+++ programmers_소영/level1/완주하지 못한 선수.test.js
@@ -0,0 +1,22 @@
+import { solution } from './완주하지 못한 선수';
+
+describe('완주하지 못한 선수 test', () => {
+  test('solution test case 1', () => {
+    const participant = ['leo', 'kiki', 'eden'];
+    const completion = ['eden', 'kiki'];
+    const result = 'leo';
+    expect(solution(participant, completion)).toBe(result);
+  });
+  test('solution test case 2', () => {
+    const participant = ['marina', 'josipa', 'nikola', 'vinko', 'filipa'];
+    const completion = ['josipa', 'filipa', 'marina', 'nikola'];
+    const result = 'vinko';
+    expect(solution(participant, completion)).toBe(result);
+  });
+  test('solution test case 3', () => {
+    const participant = ['mislav', 'stanko', 'mislav', 'ana'];
+    const completion = ['stanko', 'ana', 'mislav'];
+    const result = 'mislav';
+    expect(solution(participant, completion)).toBe(result);
+  });
+})
