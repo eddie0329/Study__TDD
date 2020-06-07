cab83b3b1e996dcf2dcc01a25d991a340ffd7a8c
soyoung <soyoungp02@gmail.com>
2020-06-08 00:58:09 +0900

    ✨ Solve Problems

--- programmers_소영/level1/완주하지 못한 선수.js
+++ programmers_소영/level1/완주하지 못한 선수.js
@@ -1,1 +1,12 @@
-export function solution(participant, completion) {}
+export function solution(participant, completion) {
+    const completionObj = completion.reduce((obj, player) => {
+        obj[player] = obj[player] ? obj[player] + 1 : 1;
+        return obj;
+    }, {});
+    return participant.find(player => {
+        if(completionObj[player])
+            completionObj[player] = completionObj[player]-1;
+        else 
+            return true;
+    });
+}
