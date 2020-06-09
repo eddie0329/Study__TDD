fe7034f7d03da6a1b7cf63257b23d2a7d76b719e
eddie0329 <fdsa0329@naver.com>
2020-06-10 00:10:11 +0900

    :recycle: Refactoring average to functional programming

--- programmers/level1/평균 구하기.js
+++ programmers/level1/평균 구하기.js
@@ -1,11 +1,19 @@
-export const getAverage = (sum, { length }) => {
-  return sum / length;
-};
+import { map, filter, reduce, go, curry } from "@/lib/functional-lib.js";
 
-export const addAccumulate = (accmulate, current) => {
-  return accmulate + current;
-};
+// export const getAverage = (sum, { length }) => {
+//   return sum / length;
+// };
+
+// export const addAccumulate = (accmulate, current) => {
+//   return accmulate + current;
+// };
+
+// export function solution(arr) {
+//   return getAverage(arr.reduce(addAccumulate), arr);
+// }
 
 export function solution(arr) {
-  return getAverage(arr.reduce(addAccumulate), arr);
+  const add = (a, c) => a + c;
+  const divide = (num) => num / arr.length;
+  return go(arr, reduce(add), divide);
 }
--- programmers/level1/평균 구하기.test.js
+++ programmers/level1/평균 구하기.test.js
@@ -5,14 +5,14 @@ describe("평균 구하기 test", () => {
     expect(solution([1, 2, 3, 4])).toEqual(2.5);
     expect(solution([5, 5])).toEqual(5);
   });
-  it("addAccumulate Test", () => {
-    expect(addAccumulate(2, 3)).toBe(5);
-    expect(addAccumulate(2, 4)).toBe(6);
-    expect(addAccumulate(3, 4)).toBe(7);
-  });
-  it("getAverage Test", () => {
-    expect(getAverage(10, [1, 2])).toBe(5);
-    expect(getAverage(3, [1, 2, 3])).toBe(1);
-    expect(getAverage(20, [1, 2])).toBe(10);
-  });
+  // it("addAccumulate Test", () => {
+  //   expect(addAccumulate(2, 3)).toBe(5);
+  //   expect(addAccumulate(2, 4)).toBe(6);
+  //   expect(addAccumulate(3, 4)).toBe(7);
+  // });
+  // it("getAverage Test", () => {
+  //   expect(getAverage(10, [1, 2])).toBe(5);
+  //   expect(getAverage(3, [1, 2, 3])).toBe(1);
+  //   expect(getAverage(20, [1, 2])).toBe(10);
+  // });
 });
