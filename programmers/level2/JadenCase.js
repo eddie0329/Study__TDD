3121ad747facde3977644afa4acfd6d3ca690841
eddie0329 <fdsa0329@naver.com>
2020-06-14 23:18:45 +0900

    :white_check_mark: Add new test

--- programmers/level2/JadenCase.js
+++ programmers/level2/JadenCase.js
@@ -1,1 +1,8 @@
-export function solution(s) {}
+export function solution(s) {
+  const stringArray = s.toLowerCase().split(" ");
+  const newArray = stringArray.map((word, index) => {
+    const newWord = word[0].toUpperCase() + word.slice(1);
+    return newWord;
+  });
+  return newArray.join(" ");
+}
--- programmers/level2/JadenCase.test.js
+++ programmers/level2/JadenCase.test.js
@@ -2,7 +2,8 @@ import { solution } from "./JadenCase";
 
 describe("Jaden Case", () => {
   it("solution test", () => {
-    expect(solution("3people unFollowed me")).toEqual("3 people Unfollowed Me");
+    expect(solution("3people unFollowed me")).toEqual("3people Unfollowed Me");
     expect(solution("for the last week")).toEqual("For The Last Week");
+    expect(solution("hello World ")).toEqual("Hello World ");
   });
 });
