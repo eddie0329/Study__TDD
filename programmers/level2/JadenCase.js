b14954b1eee90cd09deea30f7ad7e29c0a11c797
eddie0329 <fdsa0329@naver.com>
2020-06-14 23:24:19 +0900

    :sparkles: Solve jaden case

--- programmers/level2/JadenCase.js
+++ programmers/level2/JadenCase.js
@@ -1,7 +1,10 @@
 export function solution(s) {
-  const stringArray = s.toLowerCase().split(" ");
+  const stringArray = s.split(" ");
   const newArray = stringArray.map((word, index) => {
-    const newWord = word[0].toUpperCase() + word.slice(1);
+    if (word === "") {
+      return "";
+    }
+    const newWord = word[0].toUpperCase() + word.toLowerCase().slice(1);
     return newWord;
   });
   return newArray.join(" ");
