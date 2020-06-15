51559d44464482bc3b1ae87a6f5526919e4ad291
soyoung <soyoungp02@gmail.com>
2020-06-16 00:33:36 +0900

    ✨ Solve Problems

--- programmers_소영/level1/평균 구하기.js
+++ programmers_소영/level1/평균 구하기.js
@@ -1,1 +1,8 @@
-export function solution(arr) {}
+export function solution(arr) {
+  const size = arr.length;
+    let sum = 0;
+    for (let i=0; i<size; i++) {
+        sum += arr[i];
+    }
+    return sum/size;
+}
