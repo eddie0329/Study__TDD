7d5b2e89bb728fcacd1b75aeb72bd870d62d536d
eddie <fdsa0329@naver.com>
2020-06-28 00:21:58 +0900

    :wrench: Change config by adding jest-chain

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,5 @@ module.exports = {
     "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
   ],
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["jest-chain"],
 };
--- phase1/sum.test.js
+++ phase1/sum.test.js
@@ -2,4 +2,5 @@ import { sum } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+  expect([1, 2, 3]).toHaveLength(3).toEqual([1, 2, 3]);
 });
