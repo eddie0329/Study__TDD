a4c47fa3b7c35374a1f6317a532546165bcd03ac
soyoung park <soyoungp02@gmail.com>
2020-06-30 12:01:42 +0900

    ♻️ Refactor solution

--- programmers_소영/level1/완주하지 못한 선수.js
+++ programmers_소영/level1/완주하지 못한 선수.js
@@ -1,5 +1,14 @@
 export function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    return participant.find((player, i) => completion[i] != player);
+  participant.sort();
+  completion.sort();
+  return findUnmatchedPlayer(participant, completion);
+}
+
+function findUnmatchedPlayer(participant, completion) {
+  for (let i = 0; i < participant; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
+    return null;
+  }
 }
