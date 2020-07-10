6bd24b60de8f8661555e0ab18d87f2f3e8243b7f
eddie0329 <fdsa0329@naver.com>
2020-07-10 14:23:42 +0900

    :white_check_mark: Add e2e tests

--- vue-test-practice/tests/e2e/specs/test.js
+++ vue-test-practice/tests/e2e/specs/test.js
@@ -3,6 +3,18 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.cget('plusButton').fclick();
+    cy.cget('plusButton').fclick();
+    cy.cget('plusButton').fclick();
+    cy.cget('plusButton').fclick();
+    cy.cget('plusButton').fclick();
+    cy.ccontains('countDisplay', 5);
+    cy.cget('countDisplay').should('have.text', '5');
+    cy.cget('minusButton').fclick();
+    cy.cget('minusButton').fclick();
+    cy.cget('minusButton').fclick();
+    cy.cget('minusButton').fclick();
+    cy.cget('minusButton').fclick();
+    cy.cget('countDisplay').should('have.text', '0');
   });
 });
