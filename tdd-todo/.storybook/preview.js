ed1f5c8c2aee5f0aa71f149d3dd4995258c192fd
eddie0329 <fdsa0329@naver.com>
2020-07-13 12:31:42 +0900

    :sparkles: Add element ui settings in storybook

--- tdd-todo/.storybook/preview.js
+++ tdd-todo/.storybook/preview.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/vue';
 import Vue from 'vue';
-import MyCounter from '../src/components/MyCounter';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.component('my-counter', MyCounter);
+Vue.use(ElementUI);
+
 
 configure(require.context('../src/', true, /\.stories\.js$/), module);
