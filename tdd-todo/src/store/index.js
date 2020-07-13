d3ff858378b129b3f47ba6b0e88bf62e7cc30e30
eddie0329 <fdsa0329@naver.com>
2020-07-13 15:23:32 +0900

    :sparkles: Add todo stores

--- tdd-todo/src/store/index.js
+++ tdd-todo/src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import todos from './modules/todos';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    todos,
   },
 });
