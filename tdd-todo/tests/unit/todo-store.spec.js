c8597eb32c1ce5a2e4d4e2c9c19554fe629bda00
eddie0329 <fdsa0329@naver.com>
2020-07-13 15:48:54 +0900

    :white_check_mark: Add SET_TODO test

--- tdd-todo/tests/unit/todo-store.spec.js
+++ tdd-todo/tests/unit/todo-store.spec.js
@@ -3,16 +3,14 @@ import mockData from '../__mock__/todo-mock';
 
 const { mutations } = todosStore;
 
-const { ADD_TODO } = mutations;
+const { SET_TODOS } = mutations;
 
 describe('Mutation test', () => {
-  describe('ADD_TODO test', () => {
-    it('test1 add -> state = todos: { title: test1, ...}', () => {
+  describe('SET_TODO test', () => {
+    it('set todos -> state = todos: { title: test1, ...}', () => {
       const state = { todos: {} };
-      ADD_TODO(state, mockData.singleMockTodo);
-      expect(state.todos).toEqual({
-        1: { title: 'test1', id: 1, isTodoDone: false },
-      });
+      SET_TODOS(state, mockData.singleItemTodos);
+      expect(state.todos).toEqual(mockData.singleItemTodos);
     });
   });
 });
