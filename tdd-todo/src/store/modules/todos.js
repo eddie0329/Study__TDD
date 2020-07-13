e595e2fff88cd414d587f8c6a051f1c8b035cd7d
eddie0329 <fdsa0329@naver.com>
2020-07-13 19:27:55 +0900

    :sparkles: Add mutation ADD_TODO_ID

--- tdd-todo/src/store/modules/todos.js
+++ tdd-todo/src/store/modules/todos.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-underscore-dangle */
 const _state = {
   todos: {},
+  todoIds: [],
 };
 
 const _getters = {};
 
 const SET_TODOS = 'SET_TODOS';
+const ADD_TODO_ID = 'ADD_TODO_ID';
 
 const _mutations = {
   [SET_TODOS](state, todos) {
     state.todos = todos;
+  },
+
+  [ADD_TODO_ID](state, id) {
+    state.todoIds.unshift(id);
   },
 };
 
