462a41586ad4951724de1d6f2ec7989509cefb8d
eddie0329 <fdsa0329@naver.com>
2020-07-13 19:58:21 +0900

    :pencil: Write comments

--- tdd-todo/src/store/modules/todos.js
+++ tdd-todo/src/store/modules/todos.js
@@ -6,24 +6,45 @@ const _state = {
 
 const _getters = {};
 
+// MUTATION TYPES
 const SET_TODOS = 'SET_TODOS';
 const DELETE_TODO = 'DELETE_TODO';
 const ADD_TODO_ID = 'ADD_TODO_ID';
 const DELETE_TODO_ID = 'DELETE_TODO_ID';
 
 const _mutations = {
+  /**
+   *
+   * @param {object} state
+   * @param {object} todos
+   */
   [SET_TODOS](state, todos) {
     state.todos = todos;
   },
 
+  /**
+   *
+   * @param {object} state
+   * @param {string} id
+   */
   [DELETE_TODO](state, id) {
     delete state.todos[id];
   },
 
+  /**
+   *
+   * @param {object} state
+   * @param {string} id
+   */
   [ADD_TODO_ID](state, id) {
     state.todoIds.unshift(id);
   },
 
+  /**
+   *
+   * @param {object} state
+   * @param {number} index
+   */
   [DELETE_TODO_ID](state, index) {
     state.todoIds.splice(index, 1);
   },
