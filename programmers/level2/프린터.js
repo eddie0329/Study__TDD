072e0ce59fd46dc2b1d0ca54ac398c273e68f8b0
eddie <fdsa0329@naver.com>
2020-07-19 00:50:34 +0900

    :sparkles: Solve printer

--- programmers/level2/프린터.js
+++ programmers/level2/프린터.js
@@ -2,4 +2,22 @@
  * {array} priorities
  * {number} location
  */
-export const solution = (priorties, location) => {};
+export const solution = (priorties, location) => {
+  const stack = [];
+  const ADJUSTMENT_FOR_PRIORITY_VALUE = 1;
+  priorties.forEach((priority, index) => {
+    const temp = { priority, index };
+    stack.push(temp);
+  });
+  stack.sort((a, b) => {
+    return a.priority - b.priority;
+  });
+  stack.reverse();
+  return (
+    stack.findIndex((priorty) => {
+      if (priorty.index === location) {
+        return true;
+      }
+    }) + ADJUSTMENT_FOR_PRIORITY_VALUE
+  );
+};
