5e5ccbf84b8199063c82136298056c53a83b57df
eddie0329 <fdsa0329@naver.com>
2020-07-19 22:04:19 +0900

    :sparkles: Solve printer

--- programmers/level2/프린터.js
+++ programmers/level2/프린터.js
@@ -2,20 +2,33 @@
  * {array} priorities
  * {number} location
  */
-export const solution = (priorties, location) => {
-  const stack = [];
-  const ADJUSTMENT_FOR_PRIORITY_VALUE = 1;
-  priorties.forEach((priority, index) => {
-    const temp = { priority, index };
-    stack.push(temp);
-  });
-  stack.sort((a, b) => {
-    return a.priority - b.priority;
-  });
-  stack.reverse();
-  return stack.findIndex((priorty) => {
-    if (priorty.index === location) {
-      return true;
+export const solution = (priorities, location) => {
+  let answer = 0;
+  const ADJUSTMENT_FOR_LOCATION = 1;
+
+  const refinedPriorities = priorities.reduce((a, c, index) => {
+    const temp = {};
+    temp["priority"] = c;
+    temp["index"] = index;
+    a.push(temp);
+    return a;
+  }, []);
+
+  while (true) {
+    const MAX = Math.max(...refinedPriorities.map((p) => p.priority));
+    // IF MAX
+    if (refinedPriorities[0].priority === MAX) {
+      // IF TARGET VALUE
+      if (location === refinedPriorities[0].index) {
+        break;
+      } else {
+        answer += 1;
+        refinedPriorities.shift();
+      }
+    } else {
+      // IF NOT MAX
+      refinedPriorities.push(refinedPriorities.shift());
     }
-  });
+  }
+  return answer + ADJUSTMENT_FOR_LOCATION;
 };
