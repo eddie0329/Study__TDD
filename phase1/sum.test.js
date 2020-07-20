0fc547f2d113a32839f7059dae011512e957cff7
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-07-20 17:20:27 +0900

    :bulb: Add example comments for jest chian and jest expect message

--- phase1/sum.test.js
+++ phase1/sum.test.js
@@ -2,6 +2,8 @@ import { sum } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+  // jest chain example
   expect([1, 2, 3]).toHaveLength(3).toEqual([1, 2, 3]);
+  // jest expect message example
   expect(sum(1, 2), 'this should be 3').toBe(3);
 });
