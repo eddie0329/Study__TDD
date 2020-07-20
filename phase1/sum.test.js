c4054309cbe22c29ac642d39b4778b6220ca531a
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-07-20 17:19:03 +0900

    :wrench: Change config to add jest-expect-message

--- phase1/sum.test.js
+++ phase1/sum.test.js
@@ -3,4 +3,5 @@ import { sum } from "./sum";
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
   expect([1, 2, 3]).toHaveLength(3).toEqual([1, 2, 3]);
+  expect(sum(1, 2), 'this should be 3').toBe(3);
 });
