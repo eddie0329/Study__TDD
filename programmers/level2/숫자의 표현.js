9880f359111b4463fb13fc6c2ef743ee5df7517b
eddie0329 <fdsa0329@naver.com>
2020-07-23 21:36:31 +0900

    :sparkles: Solve 숫자의 표현

--- programmers/level2/숫자의 표현.js
+++ programmers/level2/숫자의 표현.js
@@ -2,4 +2,25 @@
  *
  * @param {number} n
  */
-export function solution(n) {}
+export function solution(n) {
+  let answer = 0;
+  for (let i = 1; i <= n; i += 1) {
+    let sum = i;
+    console.log("i", sum);
+    for (let j = i + 1; j <= n + 1; j += 1) {
+      console.log("j", j);
+      console.log("sum", sum);
+      if (sum === n) {
+        answer += 1;
+        break;
+      }
+      if (sum < n) {
+        sum += j;
+      }
+      if (sum > n) {
+        break;
+      }
+    }
+  }
+  return answer;
+}
