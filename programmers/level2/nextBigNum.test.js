f6c44be5298b6aeba37944594796c32fb44cf3cc
eddie <fdsa0329@naver.com>
2020-08-01 19:53:27 +0900

    :white_check_mark: Add test next big num

---
+++ programmers/level2/nextBigNum.test.js
@@ -0,0 +1,12 @@
+import { solution } from "./다음 큰 숫자";
+
+describe("다음 큰 숫자 test", () => {
+  describe("solution test", () => {
+    it("78 -> 83", () => {
+      expect(solution(78)).toBe(83);
+    });
+    it("15 -> 23", () => {
+      expect(solution(15)).toBe(23);
+    });
+  });
+});
