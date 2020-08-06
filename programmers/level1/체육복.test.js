6268b520af07474761cc484b7c07ce6f4a4eedcd
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-08-06 17:39:06 +0900

    :white_check_mark: Update pe test

--- programmers/level1/체육복.test.js
+++ programmers/level1/체육복.test.js
@@ -11,5 +11,20 @@ describe("체육복 test", () => {
     it("test3", () => {
       expect(solution(3, [3], [1])).toBe(2);
     });
+    it('test4', () => {
+      expect(3, [1, 3], [1, 2]).toBe(3);
+    });
+    it('test5', () => {
+      expect(4, [1], [1]).toBe(4);
+    });
+    it('test6', () => {
+      expect(5, [2, 3, 4], [3, 4, 5]).toBe(5);
+    });
+    it('test7', () => {
+      expect(10, [3, 9, 10], [3, 8, 9]).toBe(10);
+    })
+    it('test8', () => {
+      expect(5, [3, 4], [4, 5]).toBe(4);
+    })
   });
 });
