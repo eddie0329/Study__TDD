59f1d3672aefe280539671585fbf210cb8902a59
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-08-06 18:32:36 +0900

    :construction: Working in progress pe

--- programmers/level1/체육복.js
+++ programmers/level1/체육복.js
@@ -5,12 +5,22 @@
  * @param {array} reserve 
  */
 export const solution = (n, lost, reserve) => {
-  lost.forEach((l) => {
-    const findMine = reserve.findIndex(r => r === l);
-    if (findMine !== -1) {
-      console.log(l);
-      reserve.splice(findMine, 1);
+  const filteredLost = lost.filter(l => {
+    const findIdentical = reserve.findIndex(r => r === l);
+    if (findIdentical !== -1) {
+      return false;
     }
+    console.log('i', findIdentical);
+    reserve.splice(findIdentical, 1);
+    // console.log('l', l);
+    // console.log('index', findIdentical);
+    // console.log('lost', lost);
+    // console.log('reserve', reserve);
+    return true;
+  });
+  console.log(filteredLost);
+  console.log(reserve);
+  filteredLost.forEach((l) => {
     const findBorrowBelow = reserve.findIndex(r => r === l - 1);
     if (findBorrowBelow === -1) {
       const findBorrowUpper = reserve.findIndex(r => r === l + 1);
--- programmers/level1/체육복.test.js
+++ programmers/level1/체육복.test.js
@@ -2,29 +2,29 @@ import { solution } from "./체육복";
 
 describe("체육복 test", () => {
   describe("solution test", () => {
-    it("test1", () => {
-      expect(solution(5, [2, 4], [1, 3, 5])).toBe(5);
-    });
-    it("test2", () => {
-      expect(solution(5, [2, 4], [3])).toBe(4);
-    });
-    it("test3", () => {
-      expect(solution(3, [3], [1])).toBe(2);
-    });
-    it('test4', () => {
-      expect(3, [1, 3], [1, 2]).toBe(3);
-    });
-    it('test5', () => {
-      expect(4, [1], [1]).toBe(4);
-    });
-    it('test6', () => {
-      expect(5, [2, 3, 4], [3, 4, 5]).toBe(5);
-    });
-    it('test7', () => {
-      expect(10, [3, 9, 10], [3, 8, 9]).toBe(10);
-    })
+    // it("test1", () => {
+    //   expect(solution(5, [2, 4], [1, 3, 5])).toBe(5);
+    // });
+    // it("test2", () => {
+    //   expect(solution(5, [2, 4], [3])).toBe(4);
+    // });
+    // it("test3", () => {
+    //   expect(solution(3, [3], [1])).toBe(2);
+    // });
+    // it('test4', () => {
+    //   expect(solution(3, [1, 3], [1, 2])).toBe(3);
+    // });
+    // it('test5', () => {
+    //   expect(solution(4, [1], [1])).toBe(4);
+    // });
+    // it('test6', () => {
+    //   expect(solution(5, [2, 3, 4], [3, 4, 5])).toBe(5);
+    // });
+    // it('test7', () => {
+    //   expect(solution(10, [3, 9, 10], [3, 8, 9])).toBe(10);
+    // })
     it('test8', () => {
-      expect(5, [3, 4], [4, 5]).toBe(3);
+      expect(solution(5, [3, 4], [4, 5])).toBe(4);
     })
   });
 });
