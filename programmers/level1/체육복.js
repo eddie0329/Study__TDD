a255f4d3a6776a961aa09721dab7bd4ddd6742ff
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-08-07 16:41:38 +0900

    :recycle: Refactor isMinusOne code

--- programmers/level1/체육복.js
+++ programmers/level1/체육복.js
@@ -4,7 +4,6 @@
  */
 export const isMinusOne = (num) => num === -1;
 
-
 /**
  * 
  * @param {number} n 
@@ -14,18 +13,18 @@ export const isMinusOne = (num) => num === -1;
 export const solution = (n, lost, reserve) => {
   const filteredLost = lost.filter(l => {
     const findIdentical = reserve.findIndex(r => r === l);
-    if (findIdentical !== -1) {
-      reserve.splice(findIdentical, 1);
-      return false;
-    } else {
+    if (isMinusOne(findIdentical)) {
       return true;
+    } else {
+      reserve.splice(findIdentical, 1);
+      return false
     }
   });
   filteredLost.forEach((l) => {
     const findBorrowBelow = reserve.findIndex(r => r === l - 1);
-    if (findBorrowBelow === -1) {
+    if (isMinusOne(findBorrowBelow)) {
       const findBorrowUpper = reserve.findIndex(r => r === l + 1);
-      if (findBorrowUpper === -1) {
+      if (isMinusOne(findBorrowUpper)) {
         n -= 1;
       } else {
         reserve.splice(findBorrowUpper, 1);
