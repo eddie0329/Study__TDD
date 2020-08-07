54e0f6e123ac432cf33571c5deff77a7327123a5
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-08-07 11:18:06 +0900

    :sparkles: Add MyCounter2

---
+++ vue-test-practice/src/components/MyCounter2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>{{ count }}</h2>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <br />
+    <br />
+    <hr />
+    <div>{{ items }}</div>
+    <button @click="fetchItems">FETCH!!</button>
+  </div>
+</template>
+
+<script>
+import { INCREMENT, DECREMENT, FETCH_ITEMS } from '../store/modules/counter';
+import { mapState, mapMutations, mapActions } from 'vuex';
+
+export default {
+  name: 'MyCounter',
+  computed: {
+    ...mapState('counter', ['count', 'items']),
+  },
+  methods: {
+    ...mapMutations('counter', { increment: INCREMENT, decrement: DECREMENT }),
+    ...mapActions('counter', { fetchItems: FETCH_ITEMS }),
+  },
+};
+</script>
+
+<style></style>
