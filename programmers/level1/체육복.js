a4997f79d566c3cc70e3cff3827878f83378d7ac
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-08-07 16:30:59 +0900

    :sparkles: Implement solution for pe

--- programmers/level1/체육복.js
+++ programmers/level1/체육복.js
@@ -8,19 +8,12 @@ export const solution = (n, lost, reserve) => {
   const filteredLost = lost.filter(l => {
     const findIdentical = reserve.findIndex(r => r === l);
     if (findIdentical !== -1) {
+      reserve.splice(findIdentical, 1);
       return false;
     } else {
-      console.log('i', findIdentical);
-      reserve.splice(findIdentical, 1);
-      // console.log('l', l);
-      // console.log('index', findIdentical);
-      // console.log('lost', lost);
-      // console.log('reserve', reserve);
       return true;
     }
   });
-  console.log(filteredLost);
-  console.log(reserve);
   filteredLost.forEach((l) => {
     const findBorrowBelow = reserve.findIndex(r => r === l - 1);
     if (findBorrowBelow === -1) {
